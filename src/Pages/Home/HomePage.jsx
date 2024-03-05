6b42048eafd3f459435f32afca36e3326b7ee6c9
tomlukrprogram <tomlukeprogramming@outlook.com>
2024-03-05 12:03:55 +0000

    added placeholder image, creating better website structure

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -4,13 +4,23 @@ function HomePage() {
   return (
 <>
 <header>
+<div className="login">
+  <button className="login-bttn">login</button>
+  <button className='sign-up-bttn'>sign up</button>
+</div>
 
 <div className='logo'> 
-<img src='#' alt='' className='logo-img' />
+<img src='src/assets/monstera-plant.jpg' alt='' className='logo-img' width='200px' />
 <h1>plant shop</h1>
 </div>
 
-<nav className='nav-bar'>nav bar</nav>
+<nav className='nav-bar'>
+  <span>home</span>
+  <span>shop</span>
+  <span>about us</span>
+  <span>account</span>
+  <span>contact us</span>
+</nav>
 
 </header>
 
