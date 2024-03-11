e3fe7ef0fd4ff8f907d2e25da73d5e0ea1a3821a
tomlukewarden <tomlukeprogramming@outlook.com>
2024-03-11 16:10:59 +0000

    some errors but will fix this evening

--- server.js
+++ server.js
@@ -17,3 +17,59 @@ async function createUser(email, password, firstName, lastName) {
   }
 }
 
+async function createOrder(userId, plantIds, totalPrice) {
+  try {
+    const newOrder = await prisma.order.create({
+      data: {
+        user: {
+          connect: { id: userId },
+        },
+        plants: {
+          connect: plantIds.map((id) => ({ id })),
+        },
+        totalPrice,
+      },
+    });
+    return newOrder;
+  } catch (error) {
+    throw new Error("Failed to create order.");
+  }
+}
+
+async function createPlant(name, description, price, image, inventory) {
+  try {
+    const newPlant = await prisma.plant.create({
+      data: {
+        name,
+        description,
+        price,
+        image,
+        inventory,
+      },
+    });
+    return newPlant;
+  } catch (error) {
+    throw new Error("Failed to create plant: " + error.message);
+  }
+}
+
+async function updatePlant(id, data) {
+  try {
+    const updatedPlant = await prisma.plant.update({
+      where: { id },
+      data,
+    });
+    return updatedPlant;
+  } catch (error) {
+    throw new Error("Failed to update plant: " + error.message);
+  }
+}
+
+async function getAllPlants() {
+  try {
+    const plants = await prisma.plant.findMany();
+    return plants;
+  } catch (error) {
+    throw new Error("Failed to fetch plants: " + error.message);
+  }
+}
