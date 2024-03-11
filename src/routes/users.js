13e75be6127ec90d39cb6cb2d8a4f9f4b1f605cd
tomlukewarden <tomlukeprogramming@outlook.com>
2024-03-11 17:13:41 +0000

    routes/users.js changed slightly

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,25 @@
-const express = require('express')
-const router = express.Router()
-const { createUser } = require('../controller/users')
+const express = require('express');
+const router = express.Router();
+const { createUser, getAllUsers } = require('../controller/users');
+
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await getAllUsers();
+    res.status(200).json(users); 
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 
 router.post('/', async (req, res) => {
-  try{
-  const newUser = await createUser(req.body)
-  res.status(201).json(newUser) 
+  try {
+    const newUser = await createUser(req.body);
+    res.status(201).json(newUser);
   } catch (error) {
-    res.status(500).json({ error:
-    error.message})
+    res.status(500).json({ error: error.message });
   }
-})
+});
+
+module.exports = router;
